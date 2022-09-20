b1c131be1cfc483254938775eaf6c1d3776b090c
NerminMemmedova5 <nerminmemmedova379@gmail.com>
2022-09-20 12:48:06 +0400

    Add project files.

---
+++ WindowsFormsApp7/JsonHelper.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace WindowsFormsApp7
+{
+    public static class JsonHelper
+    {
+        public static void Serialize(Person person)
+        {
+            var serializer = new JsonSerializer();
+            using (var sw = new StreamWriter("person.json"))
+            {
+                using (var jw = new JsonTextWriter(sw))
+                {
+                    jw.Formatting = Formatting.Indented;
+                    serializer.Serialize(jw, person);
+                }
+            }
+
+        }
+    }
+}
---
+++ WindowsFormsApp7/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp7
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public bool IsMale { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
+    }
+}
